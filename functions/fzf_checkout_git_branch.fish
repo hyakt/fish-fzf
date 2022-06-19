803b03f22d9fff5b7676eb03962932f27268bc77
hyakt <hyakt0@gmail.com>
2022-06-19 14:15:52 +0900

    Add fzf_checkout_git_branch.fish

---
+++ functions/fzf_checkout_git_branch.fish
@@ -0,0 +1,8 @@
+function fzf_checkout_git_branch
+    git branch | fzf --preview "git log --first-parent --graph --abbrev-commit --decorate {1}" | sed -e 's/^..\(.*\)/\1/' | read branch_name
+    if test -n "$branch_name"
+        echo "Swith branch to $branch_name"
+        git checkout $branch_name
+    end
+    commandline -f repaint
+end
