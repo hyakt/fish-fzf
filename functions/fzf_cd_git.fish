5dcedad129179c0f1185a1c6771cb6efd4200e14
hyakt <hyakt0@gmail.com>
2022-06-19 14:56:11 +0900

    Add fzf_cd_git.fish

---
+++ functions/fzf_cd_git.fish
@@ -0,0 +1,8 @@
+function fzf_cd_git
+    set base_git_dir (git rev-parse --show-superproject-working-tree --show-toplevel)
+    fd --type d --base-directory $base_git_dir | fzf --preview "ls -l (git rev-parse --show-superproject-working-tree --show-toplevel)/{}" | read directory
+    if [ $directory ]
+        cd $base_git_dir/$directory;
+    end
+    commandline -f repaint
+end
